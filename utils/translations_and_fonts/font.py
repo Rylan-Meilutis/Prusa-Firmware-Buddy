Rylan-Meilutis/Prusa-Firmware-Buddy

commit e1154aa5272650caed33b260d6a21b2e2e1428d0

Optimize storage for fonts with odd width
No need to store empty half-byte on each row.
This saves about 9kB of FLASH on MK4.

This also improves speed quite a bit.
Typical values now are as follows:

  ~4980 cycles for  9x16 font
  ~6568 cycles for 11x19 font

utils/translations_and_fonts/font.py
@@ -21,8 +21,7 @@ def png2font(src_img, char_w, char_h, columns, rows):
 
     # Initialize charset data with zeros
     # Constants for 4 bits per pixel (2 pixels per byte)
-    char_bpr = (char_w + 1) >> 1  # bytes per row
-    char_size = char_h * char_bpr  # character size in bytes
+    char_size = (char_h * char_w + 1) >> 1  # character size in bytes
     char_count = columns * rows  # character count
     charset_size = char_size * char_count  # charset size in bytes
     charset_data = bytearray(charset_size)
@@ -41,12 +40,11 @@ def png2font(src_img, char_w, char_h, columns, rows):
             char_offs = char_code * char_size  # character offset in charset [bytes]
             char_x = x % char_w  # character pixel x-coord (0..char_w-1)
             char_y = y % char_h  # character pixel y-coord (0..char_h-1)
-            char_row_offs = char_y * char_bpr  # character row offset [bytes]
-            char_pix_offs = char_x >> 1  # character pixel offset [bytes]
-            offs = char_offs + char_row_offs + char_pix_offs  # total offset in charset [bytes]
+            char_pix_offs = char_y * char_w + char_x  # character pixel offset [pixels]
+            offs = char_offs + char_pix_offs >> 1  # total offset in charset [bytes]
 
             # Calculate bit shift (4 bits per pixel, 2 pixels per byte)
-            i = char_x % 2
+            i = char_pix_offs % 2
             i = 4 - (i * 4)
 
             # Update character pixel data

commit 7fc220fea4481d51f875f98a2ed4e4e1332d5a55

fonts: Fix byte offset computation in png2font
Bug was introduced in e1154aa5272650caed33b260d6a21b2e2e1428d0
and was dormant because generated headers were checked in.

BFW-9130

utils/translations_and_fonts/font.py
@@ -41,7 +41,7 @@ def png2font(src_img, char_w, char_h, columns, rows):
             char_x = x % char_w  # character pixel x-coord (0..char_w-1)
             char_y = y % char_h  # character pixel y-coord (0..char_h-1)
             char_pix_offs = char_y * char_w + char_x  # character pixel offset [pixels]
-            offs = char_offs + char_pix_offs >> 1  # total offset in charset [bytes]
+            offs = char_offs + (char_pix_offs >> 1)  # total offset [bytes]
 
             # Calculate bit shift (4 bits per pixel, 2 pixels per byte)
             i = char_pix_offs % 2